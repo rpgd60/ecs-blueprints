477a0c5d2642f0150f6ff9e335fdcc38cad68cab
Rafa <rafa@portillo.eu>
2024-11-16 13:40:00 +0100

    arn for task definition

--- terraform/fargate-examples/lb-service/outputs.tf
+++ terraform/fargate-examples/lb-service/outputs.tf
@@ -2,3 +2,8 @@ output "application_url" {
   value       = "http://${module.alb.dns_name}"
   description = "Copy this value in your browser in order to access the deployed app"
 }
+
+output "task_definition" {
+  value = module.ecs_service.task_definition_arn
+  description = "Task definition ARN"
+}
